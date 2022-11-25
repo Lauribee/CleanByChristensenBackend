96049815ecd54be53c2f6a5b8e77fa680a95f514
Beeboo <laura_stroe_2001@yahoo.com>
2022-11-25 15:04:34 +0100

    første commit

---
+++ src/main/java/com/example/cleanbychristensenbackend/CleanByChristensenBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.cleanbychristensenbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CleanByChristensenBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CleanByChristensenBackendApplication.class, args);
+    }
+
+}
